783a3a982ea79565805edb0ba0f7e7f9627877ce
benNjogu <kamandemachafi@gmail.com>
2022-03-15 04:10:56 +0300

    var keyword

--- Part1:Introduction/index.js
+++ Part1:Introduction/index.js
@@ -1,2 +1,12 @@
 
+function sayHello() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
 
+    console.log(i);
+}
+
+sayHello();
+
+//with var i is accessible outside the for loop
