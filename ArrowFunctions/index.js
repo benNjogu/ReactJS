19672a687585ae75ddc2e9481e496e446f963272
benNjogu <kamandemachafi@gmail.com>
2022-03-15 04:52:24 +0300

    Filtering an array

--- ArrowFunctions/index.js
+++ ArrowFunctions/index.js
@@ -1,8 +1,9 @@
 
-const square = function (number) {
-    return number * number;
-}
+const jobs = [
+    { id: 1, isActive: true },
+    { id: 2, isActive: true },
+    { id: 3, isActive: false }
+]
 
-const square1 = number => number * number;
-
-console.log(square1(9))//81
+const activeJobs = jobs.filter(job => job.isActive);
+console.log(activeJobs);
