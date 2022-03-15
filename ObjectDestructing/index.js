4ea9e809dc8e2b79635c732b4ed6f39be52b50c1
benNjogu <kamandemachafi@gmail.com>
2022-03-15 20:44:38 +0300

    Object destructing.

--- ObjectDestructing/index.js
+++ ObjectDestructing/index.js
@@ -1,2 +1,13 @@
 
+const address = {
+    street: '',
+    city: '',
+    country: ''
+};
 
+const street = address.street;
+const city = address.city;
+const country = address.country;
+
+//The above three lines can be simplified as below.
+const { street: st, city, country } = address;
