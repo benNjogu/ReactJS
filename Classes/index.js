137084445ce27675ab952ce8d27e298e8bcafa08
benNjogu <kamandemachafi@gmail.com>
2022-03-16 10:44:39 +0300

    Modules

--- Classes/index.js
+++ Classes/index.js
@@ -1,24 +1,4 @@
+import { Teacher } from "./teacher.js";
 
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
-
-    walk() {
-        console.log('walking');
-    }
-}
-
-class Teacher extends Person {
-    constructor(name, degree) {
-        super(name);
-        this.degree = degree;
-    }
-
-    teach() {
-        console.log('teaching');
-    }
-}
-
-const teacher = new Teacher('Ben', 'bsc');
+const teacher = new Teacher('Abby', 'Bsc.');
 teacher.walk();
