017b9450fd6dbcb94110c6aa37933cb402c5a5e5
benNjogu <kamandemachafi@gmail.com>
2022-03-16 10:49:19 +0300

    named export

--- Classes/index.js
+++ Classes/index.js
@@ -2,3 +2,6 @@ import { Teacher } from "./teacher.js";
 
 const teacher = new Teacher('Abby', 'Bsc.');
 teacher.walk();
+
+//With the braces above around teacher, it is a great indication 
+//that it is a named export
