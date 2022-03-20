bac7534f958a34b6c08ef5b88ba529a03506b20b
benNjogu <kamandemachafi@gmail.com>
2022-03-20 03:50:37 +0300

    Specifying children.

--- counter-app/src/Components/counter.jsx
+++ counter-app/src/Components/counter.jsx
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     render() { 
-        return <h1>Hello world</h1>;
+        return(
+            <React.Fragment>//Using fragment instead of div.
+                <h1>Hello world</h1>
+                <button>Increment</button>
+            </React.Fragment>
+        )
     }
 }
  
