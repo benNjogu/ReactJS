b1871bcc8297c294c4b65748e42e0cfe20530fef
benNjogu <kamandemachafi@gmail.com>
2022-03-22 11:59:32 +0300

    Using inline styles

--- counter-app/src/Components/counter.jsx
+++ counter-app/src/Components/counter.jsx
@@ -5,16 +5,11 @@ class Counter extends Component {
     count: 0
   };
 
-  styles = {
-    fontSize: 10,
-    fontWeight: "bold"
-  };
-
   render() {
     return (
        <div>
 
-         <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+         <span style={{fontSize: 30}} className="badge badge-primary m-2">{this.formatCount()}</span>
          <button className="btn btn-secondary btn-sm">Increment</button>
        </div>
     );
