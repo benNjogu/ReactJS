09b37848050c39a714eb36f2097fa14728caff56
benNjogu <kamandemachafi@gmail.com>
2022-03-25 04:35:43 +0300

    fixing redundancy issue.

--- counter-app/src/Components/counter.jsx
+++ counter-app/src/Components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
-    id: this.props.id,
+    value: this.props.counter.value,
   };
   /**
    * props include data that we give to a component,
@@ -27,7 +26,7 @@ class Counter extends Component {
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
