b777ed7ef44cebcdde002d12d15e3be8174ace76
benNjogu <kamandemachafi@gmail.com>
2022-03-25 04:30:08 +0300

    Updating the state.

--- counter-app/src/Components/counter.jsx
+++ counter-app/src/Components/counter.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     value: this.props.value,
+    id: this.props.id,
   };
   /**
    * props include data that we give to a component,
@@ -20,13 +21,13 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
-          onClick={this.props.onDelete}
+          onClick={() => this.props.onDelete(this.props.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
