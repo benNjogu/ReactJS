6b7a41a65a267f1596cf481bd268c23c47e9d177
benNjogu <kamandemachafi@gmail.com>
2022-03-25 05:45:02 +0300

    Multiple components in sync.

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -4,10 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from './Components/counters';
 
 ReactDOM.render(
-  <Counters />,
+  <App />,
   document.getElementById('root')
 );
 
